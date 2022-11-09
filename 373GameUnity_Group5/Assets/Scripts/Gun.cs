21c4813d652090210837dcae778c2c81c1ece8d2
Pablo Vazquez <95883542+pablo123w@users.noreply.github.com>
2022-11-09 13:43:07 -0800

    added the terrain package

--- 373GameUnity_Group5/Assets/Scripts/Gun.cs
+++ 373GameUnity_Group5/Assets/Scripts/Gun.cs
@@ -9,7 +9,7 @@ public class Gun : MonoBehaviour
     public GameObject spawn;
     public float firerate;
     private float nextFireRate;
-  
+    public float speed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,7 @@ public class Gun : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(Input.GetKeyDown("1") && Time.time > nextFireRate)
         {
@@ -27,10 +27,14 @@ public class Gun : MonoBehaviour
     }
     public void Shoot()
     {
-        GameObject obj;
+       // GameObject obj;
         nextFireRate = Time.time + firerate;
-        obj = Instantiate(Gbullet);
-        obj.transform.position = spawn.transform.position;
-       
+        GameObject newProjectile = Instantiate(Gbullet);
+        newProjectile.transform.position = spawn.transform.position;
+        newProjectile.transform.rotation = transform.rotation;
+       // newProjectile.SetActive(true);
+        newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        Destroy(newProjectile, 5f);
+
     }
 }
