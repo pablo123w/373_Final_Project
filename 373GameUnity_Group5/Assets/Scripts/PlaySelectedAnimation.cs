5d4f9f67ca6fccc01e1e80eb33290cc01e890c13
Pablo Vazquez <95883542+pablo123w@users.noreply.github.com>
2022-12-07 09:17:59 -0800

    finished

---
+++ 373GameUnity_Group5/Assets/Scripts/PlaySelectedAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySelectedAnimation : MonoBehaviour
+{
+    public Animator myAnimationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("Open", false);
+        }
+    }
+}
